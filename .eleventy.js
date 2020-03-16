50d1c3f7e54e3c5cfeb6af4b6b58ddf587f3b8c8
Robert Showalter <robert@orangedaisy.co>
2020-03-16 19:23:48 -0400

    Update index with post list
    Lots going on here just to get to this point.
    
    - Switch to nunjucks from liquid because there are problems with 11ty's
      liquid library
    - Add postlist partial
    - Add library and filter to output a human readable date format since we
      lost liquid's date filter
    - Use `safe` filter when outputting markdown content because nunjucks
      requires it
    - Tweak the CSS to remove more unnecessary styles and add some more
      exploratory touches
    
    I think that about covers it. Phew!

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,15 @@
+const dateFormat = require('dateformat');
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.setDataDeepMerge(true);
+
+    // convert dates to readable format
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return dateFormat(dateObj, "fullDate");
+    });
+
+    return {
+        markdownTemplateEngine: "njk"
+    };
 };
